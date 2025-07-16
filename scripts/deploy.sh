fa161e337861b63c9dea6801851aa138532b12b9
SnehaBarai1906 <baraisneha2003@gmail.com>
2025-07-16 17:20:03 +0530

    Reorganize deploy.sh to ensure directory creation before changing to app directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "🚀 Starting deployment script..."
-cd /home/ec2-user/app         # Go to where CodeDeploy copied your files
-chmod 755 index.html          # Set permission so it’s viewable in browser or server
+
+mkdir -p /home/ec2-user/app    # Create the folder if it doesn’t exist
+cd /home/ec2-user/app          # Now cd won't fail
+chmod 755 index.html           # Only if index.html exists
+
 echo "✅ Deployment script complete."
