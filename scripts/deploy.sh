0db09d0d06ca4f9a8cb92277072c010692dc987b
SnehaBarai1906 <baraisneha2003@gmail.com>
2025-07-16 19:24:18 +0530

    Update appspec.yml and deploy.sh to change deployment destination to /home/ec2-user/app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 echo "🚀 Starting deployment script..."
-
-mkdir -p /var/www/html
-cd /var/www/html
-
-if [ -f index.html ]; then
-  chmod 755 index.html
-  echo "✅ index.html permission updated"
-else
-  echo "⚠️ index.html not found!"
-fi
-
+mkdir -p /home/ec2-user/app
+cd /home/ec2-user/app
+chmod 755 index.html
 echo "✅ Deployment script complete."
